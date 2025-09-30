91f226797493697e4cb38665f8eead40178a0985
悟空 <rainchan365@163.com>
2025-09-30 16:25:08 +0800

    Add new echo handler and update Makefile, README, and router configuration

--- internal/router/app0/router.go
+++ internal/router/app0/router.go
@@ -3,8 +3,6 @@ package app0
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
-
 	"gitee.com/huajinet/go-example/internal/common"
 )
 
@@ -17,11 +15,5 @@ func mount(app ApplicationContext) {
 	app.Engine.GET("/healthz", common.HealthzHandler())
 	app.Engine.GET("/api/v1/version", common.VersionHandler())
 
-	app.Engine.Any("/api/v1/echo", func(c *gin.Context) {
-		c.Writer.Header().Set("Content-Type", "application/json")
-		c.Writer.WriteHeader(http.StatusOK)
-		c.JSON(http.StatusOK, gin.H{
-			"headers": c.Request.Header,
-		})
-	})
+	app.Engine.Any("/api/v1/echo", Echo(app))
 }
